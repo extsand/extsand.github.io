8ee8c454058112f8c7b9f5c2e2f1850d30e20344
Alexander <extsand@gmail.com>
2020-12-10 00:39:59 +0200

    owl

---
+++ owl.handshake/app.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+	
+	const preloader = document.querySelector('.preloader')
+	setTimeout(() => {
+		if(preloader.classList.contains('preloader-active')) {
+			preloader.classList.remove('preloader-active')
+		}		
+	}, 2000);
+
+
+
+	const btnJs = document.querySelector('.logo-img')
+	const overlayHello = document.querySelector('.overlay_hello')
+	const btnLang = document.querySelector('.button__click')
+	const audio = document.getElementById('sound')
+	
+
+
+
+	btnJs.addEventListener('click', () => {
+		overlayHello.classList.add('overlay_hello-active')
+		overlayHello.addEventListener('click', () => {
+			overlayHello.classList.remove('overlay_hello-active')
+		})
+
+	})
+
+	btnLang.addEventListener('click', () => {
+		btnLang.classList.toggle('button__click-action')
+
+		// btnLang.textContent = 'No! Learn Eng'
+
+	})
+
+
+
+
+
+})
